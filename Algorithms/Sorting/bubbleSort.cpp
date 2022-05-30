fc1ae02ea835c4aa9ca78f840acd8bb13d2fa083
Abhinav <abhinavkashyap061@gmail.com>
2022-05-30 16:42:37 +0530

    Create bubbleSort.cpp

---
+++ Algorithms/Sorting/bubbleSort.cpp
@@ -0,0 +1,43 @@
+/*
+
+Bubble Sort
+
+- take larger element to the end by repeatedly swapping the adjacent elements.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort( int arr[], int n){
+
+    for( int i=0; i<=n-1; i++ ){
+
+        // repeated swapping
+        for( int j=0; j<n-1-i; j++){
+
+            // if greater -> push to the end
+            if( arr[j]>arr[j+1] ){
+
+                swap(arr[j], arr[j+1]);
+                
+            }
+
+        }
+
+    }
+}
+
+int main(){
+
+    int n;cin>>n;
+
+    int a[n]; for( int i=0; i<n; i++ ) cin>>a[i];
+
+    bubbleSort(a,n);
+
+    for( auto i: a) cout<<i<<" ";
+
+    return 0;
+}
