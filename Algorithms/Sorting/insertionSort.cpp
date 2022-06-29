bd991b52dd937275f68baf569b2be5203001d187
Abhinav <abhinavkashyap061@gmail.com>
2022-06-29 15:49:34 +0530

    Update insertionSort.cpp

--- Algorithms/Sorting/insertionSort.cpp
+++ Algorithms/Sorting/insertionSort.cpp
@@ -13,7 +13,21 @@ using namespace std;
 
 void insertionSort( int arr[], int n){
 
-    
+    for( int i=1; i<n; i++ ){
+
+        int current = arr[i];
+
+        int prev = i-1;
+
+        while( prev>=0 && arr[prev]>current ){
+
+            arr[prev+1] = arr[prev];
+
+            prev--;
+        }
+
+        arr[prev+1] = current;
+    }
     
 }
 
