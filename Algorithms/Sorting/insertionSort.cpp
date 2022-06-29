4cf89c2f52be4124f69202c9f1b8082403b4be48
Abhinav <abhinavkashyap061@gmail.com>
2022-06-29 15:29:46 +0530

    Create insertionSort.cpp

---
+++ Algorithms/Sorting/insertionSort.cpp
@@ -0,0 +1,31 @@
+/*
+
+Insertion Sort
+
+- 
+- 
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void insertionSort( int arr[], int n){
+
+    
+    
+}
+
+int main(){
+
+    int n;cin>>n;
+
+    int a[n]; for( int i=0; i<n; i++ ) cin>>a[i];
+
+    insertionSort(a,n);
+
+    for( auto i: a) cout<<i<<" ";
+
+    return 0;
+}
