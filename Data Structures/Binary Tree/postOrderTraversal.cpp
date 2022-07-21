a65e97cc3dd1754283dc56253fa1111b793cc328
Abhinav <abhinavkashyap061@gmail.com>
2022-07-21 08:59:39 +0530

    binary tree traversals added

---
+++ Data Structures/Binary Tree/postOrderTraversal.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+
+    public:
+
+        int data;
+        Node * left;
+        Node * right;
+
+        Node(int x){
+
+            data = x;
+            left = NULL;
+            right = NULL;
+
+        }
+};
+
+Node * buildTree(){
+
+    int temp;
+    cin>> temp;
+
+    if( temp==-1 ){
+
+        return NULL;
+
+    }
+
+    Node * newNode = new Node(temp);
+
+    newNode -> left = buildTree();
+
+    newNode -> right = buildTree();
+
+    return newNode;
+
+}
+
+void postOrderTraversal(Node * root){
+
+    if( root==NULL ){
+
+        return;
+
+    }
+
+    postOrderTraversal(root -> left);
+
+    postOrderTraversal(root -> right);
+
+    cout << root -> data << " ";
+
+}
+
+int main(){
+
+    Node * root = buildTree();
+
+    postOrderTraversal(root);
+
+    return 0;
+}
